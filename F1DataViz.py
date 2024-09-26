7b7e051a3263a92dfe1b436a0a78cb251770c493
wayneong2001 <Wayneong2001@gmail.com>
2024-09-26 15:07:16 -0700

    added quali data

--- F1DataViz.py
+++ F1DataViz.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import fastf1 as ff1
+from fastf1.core import Laps
+
+# load session from qualifying session from 1st race in 2024
+session = ff1.get_session(2024, 1, "Q")
+session.load()
